9e668af45fe7b8cb4c4e9346b04b00a2c98e3308
SergioTanakaAtos <sergio.tanaka@atos.net>
2023-08-02 15:28:48 -0300

    add html

--- financas/models.py
+++ financas/models.py
@@ -18,9 +18,12 @@ class Control(models.Model):
         ('Carro','Carro'),
         ('Outros','Outros')
     ]
+
     account = models.CharField(max_length=10, choices=ACCOUNTS, null=True)
     type = models.CharField(max_length=30, choices=TYPES,null=True)
     date = models.DateField(null=True)
     category = models.CharField(max_length=30,choices=CATEGORIES,null=True)
     description = models.CharField(max_length=100,null=True)
     value = models.DecimalField(max_digits=10, decimal_places=2,null=True)
+    def __str__(self):
+        return self.description
--- financas/views.py
+++ financas/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-import json
 import sqlite3
 import pandas as pd 
-
+from .models import Control
 def index(request):
     
-    df = pd.read_excel('C:\Git\Controle-financeiro\Controle_de_gastos_2023.xlsx')
-    df = df.iloc[7:23, :7]
+    df = pd.read_excel('C:\Git\Finantial_control\Controle_de_gastos_2023.xlsx')
     conn = sqlite3.connect('db.sqlite3')
     df.to_sql('financas_control', conn, if_exists='replace', index=False)
     conn.close()
-    return render(request, 'financas/index.html')
 
-def export_xls(request):
-
-    return
+    controls = Control.objects.filter()
+    print(controls)
+    return render(request, 'financas/index.html', {'controls':controls})
